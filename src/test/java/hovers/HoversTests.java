3b08be5b3699131e26fb9728a8a2c8a22f2b95d0
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2024-08-04 15:04:31 +0300

    Implementing HoverPage tests - HoversTests class✅
    Test Passed ✅

--- src/test/java/hovers/HoversTests.java
+++ src/test/java/hovers/HoversTests.java
@@ -1,7 +1,20 @@
 package hovers;
 
 import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.HoversPage;
+import static org.testng.Assert.*;
 
 public class HoversTests extends BaseTests {
 
+    @Test
+    public void testUser1(){
+        HoversPage hoversPage = homePage.clickHovers();
+        var caption = hoversPage.hoverOverFigure(1);
+        assertTrue(caption.isCaptionDisplayed(),"Caption Not Displayed");
+        assertEquals(caption.getTitle(),"name: user1","Incorrect user");
+        assertEquals(caption.getLinkText(),"View profile","Incorrect link text");
+        //assertEquals(caption.getLink().endsWith("/users/1"),"Incorrect Link");
+
+    }
 }
