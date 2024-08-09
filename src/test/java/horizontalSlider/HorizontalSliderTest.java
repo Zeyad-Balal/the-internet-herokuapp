43e09928a5cfa3f1da7cf472394d397ad9a7f111
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2024-08-09 17:58:54 +0300

    Implementing HorizontalSliderTests class. ✅
    Status -> PASSED ✅

---
+++ src/test/java/horizontalSlider/HorizontalSliderTest.java
@@ -0,0 +1,17 @@
+package horizontalSlider;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.HorizontalSliderPage;
+
+import static org.testng.Assert.assertEquals;
+public class HorizontalSliderTest extends BaseTests {
+
+    @Test
+    public void testSlideToWholeNumber(){
+        String value = "4";
+        HorizontalSliderPage sliderPage = homePage.clickHorizontalSlider();
+        sliderPage.setSliderValue(value);
+        assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
+    }
+}
