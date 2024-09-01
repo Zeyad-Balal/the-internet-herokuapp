0c77741a6587b5d496d1326caab46ef980519a71
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2024-09-01 21:27:10 +0300

    Deleting EntryAdPage Class & Its Tests Package ❌

--- src/test/java/alerts/FileUploadTests.java
+++ src/test/java/alerts/FileUploadTests.java
@@ -10,7 +10,7 @@ public class FileUploadTests extends BaseTests {
     public void testFileUpload()
     {
         FileUploadPage fileUploadPage = homePage.clickFileUpload();
-        
+
         //NOTE: in java using double backslash to avoid illegal characters error
         fileUploadPage.uploadFile("C:\\Users\\hp\\Desktop\\Resume\\Zeyad Hassan Abd Al-Halim.pdf");
         assertEquals(fileUploadPage.getFileUploaded(),"Zeyad Hassan Abd Al-Halim.pdf","unexpected file");
