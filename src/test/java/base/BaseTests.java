36b65a05eb51c9280026fedc4a92c7af7e1bb1d6
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2024-09-03 20:23:25 +0300

    Creating two @AfterMethods taking screenshots for passed & failure tests📸
    one record failure tests. one record passed tests. putting shots in resources dir. within screenshots folder🦅

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,9 +1,17 @@
 package base;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITest;
+import org.testng.ITestResult;
 import pages.HomePage;
 import org.testng.annotations.*;
 import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
     private WebDriver driver;
@@ -21,10 +29,32 @@ public class BaseTests {
     }
 
 
+    @AfterMethod
+    public void takeScreenShot()
+    {
+        var camera = (TakesScreenshot)driver;
+        File screenshot = camera.getScreenshotAs(OutputType.FILE);
+        System.out.println("Screenshot taken "+ screenshot.getAbsolutePath());
+        try{Files.move(screenshot , new File("resources/screenshots/test.png"));}
+        catch(IOException e) {e.printStackTrace();}
+    }
+
+    //for failed tests
+    @AfterMethod
+    public void testFailureRecord(ITestResult result)
+    {
+        if(ITestResult.FAILURE == result.getStatus())
+        {    var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            System.out.println("Screenshot taken "+ screenshot.getAbsolutePath());
+            try{Files.move(screenshot , new File("resources/screenshots/"+result.getName()+".png"));}
+            catch(IOException e) {e.printStackTrace();}}
+
+    }
+
     public WindowManager getWindowManager()
     {
         return new WindowManager(driver);
     }
-
 
 }
